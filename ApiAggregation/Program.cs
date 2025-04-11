470d80b1581a5d01b26565a0c1b3b386ecb0ebb9
Theodoros Pagkrakiotis <thodoris99@hotmail.com>
2025-04-11 22:51:58 +0300

    Added Serilog, Polly with Fallback, Retry, CircuitBreaker, Timeout Policies

--- ApiAggregation/Program.cs
+++ ApiAggregation/Program.cs
@@ -1,21 +1,36 @@
+using System.Net;
 using ApiAggregation.Infrastructure;
 using ApiAggregation.Services;
 using ApiAggregation.Services.Abstractions;
 using Microsoft.Extensions.Caching.Hybrid;
+using Microsoft.Extensions.Http.Resilience;
 using Microsoft.Extensions.Options;
+using Polly;
+using Polly.Fallback;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Serilog from appsettings.json
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+// Replace default logging with Serilog
+builder.Host.UseSerilog();
 
 builder.Services.AddHybridCache();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
 builder.Services.AddSingleton<IStatisticsSevice, StatisticsSevice>();
 builder.Services.AddTransient<StatisticsHandler>();
 
 builder.Services.Configure<OpenWeatherMapSettings>(builder.Configuration.GetSection("OpenWeatherMap"));
+
 builder.Services.AddHttpClient<IExternalApiClient, OpenWeatherMapClient>((serviceProvider, client) =>
     {
         var settings = serviceProvider
@@ -23,14 +38,48 @@ builder.Services.AddHttpClient<IExternalApiClient, OpenWeatherMapClient>((servic
 
         client.BaseAddress = new Uri(settings.BaseUrl);
     })
-    .AddHttpMessageHandler<StatisticsHandler>();
+    .AddHttpMessageHandler<StatisticsHandler>()
+    .AddResilienceHandler(
+        "CustomPipeline",
+        static builder =>
+        {
+            // See: https://www.pollydocs.org/strategies/retry.html
+            builder.AddRetry(new HttpRetryStrategyOptions
+            {
+                BackoffType = DelayBackoffType.Exponential,
+                MaxRetryAttempts = 5,
+                UseJitter = true
+            });
 
+            // See: https://www.pollydocs.org/strategies/circuit-breaker.html
+            builder.AddCircuitBreaker(new HttpCircuitBreakerStrategyOptions
+            {
+                // Customize and configure the circuit breaker logic.
+                SamplingDuration = TimeSpan.FromSeconds(10),
+                FailureRatio = 0.2,
+                MinimumThroughput = 3,
+                ShouldHandle = static args => ValueTask.FromResult(args is
+                {
+                    Outcome.Result.StatusCode:
+                    HttpStatusCode.RequestTimeout or
+                    HttpStatusCode.TooManyRequests
+                })
+            });
 
-builder.Services.AddHttpClient<OpenWeatherMapClient>((sp, client) =>
-{
-    var settings = sp.GetRequiredService<IOptions<OpenWeatherMapSettings>>().Value;
-    client.BaseAddress = new Uri(settings.BaseUrl);
-}).AddHttpMessageHandler<StatisticsHandler>();
+            builder.AddConcurrencyLimiter(100);
+
+            builder.AddFallback(new FallbackStrategyOptions<HttpResponseMessage>()
+            {
+                FallbackAction = _ => Outcome.FromResultAsValueTask(new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent("The service is currently unavailable. Please try again later.")
+                })
+            });
+
+            // See: https://www.pollydocs.org/strategies/timeout.html
+            builder.AddTimeout(TimeSpan.FromSeconds(5));
+        });
+
 builder.Services.Decorate<IExternalApiClient>((inner, sp) =>
     new CachingExternalApiClientDecorator(
         inner,
@@ -44,6 +93,8 @@ builder.Services.AddScoped<IAggregatorService, AggregatorService>();
 
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
